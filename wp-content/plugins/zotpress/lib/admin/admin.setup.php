4919487f4e29945a862490828725f17cd86ed762
Jeanny-ZJN <501933687@qq.com>
2023-03-21 09:48:46 +0800

    third upload

--- wp-content/plugins/zotpress/lib/admin/admin.setup.php
+++ wp-content/plugins/zotpress/lib/admin/admin.setup.php
@@ -3,7 +3,7 @@
         <div id="zp-Zotpress-Navigation">
 
             <div id="zp-Icon">
-                <img src="/dgah-capstone/wp-content/plugins/zotpress/lib/admin/<br />_<b>Fatal error</b>:  Uncaught Error: Undefined constant &quot;ZOTPRESS_PLUGIN_URL&quot; in /home/dgahsite//wp-content/plugins/zotpress/lib/admin/admin.setup.php:9_Stack trace:_#0 {main}_  thrown in <b>/home/dgahsite//wp-content/plugins/zotpress/lib/admin/admin.setup.php</b> on line <b>9</b><br />">
+                <img src="https://jeanny-zjn.github.io/dgah-capstone/wp-content/plugins/zotpress/lib/admin/<br />_<b>Fatal error</b>:  Uncaught Error: Undefined constant &quot;ZOTPRESS_PLUGIN_URL&quot; in /home/dgahsite/https://jeanny-zjn.github.io/dgah-capstone/wp-content/plugins/zotpress/lib/admin/admin.setup.php:9_Stack trace:_#0 {main}_  thrown in <b>/home/dgahsite/https://jeanny-zjn.github.io/dgah-capstone/wp-content/plugins/zotpress/lib/admin/admin.setup.php</b> on line <b>9</b><br />">
 </div>
 </div>
 </div>
